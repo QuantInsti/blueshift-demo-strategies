72369c25afbb09b0164a172a45fb743d105e213f
Prodipta Ghosh <prodiptag@gmail.com>
2022-08-01 12:30:10 +0530

    update logic

--- nse/demo_support_resistance.py
+++ nse/demo_support_resistance.py
@@ -7,7 +7,7 @@
     Asset class: Equities, Futures, ETFs and Currencies
     Broker: NSE
 """
-from blueshift_library.technicals.indicators import fibonacci_support, adx
+import talib as ta
 
 from blueshift.finance import commission, slippage
 from blueshift.api import(  symbol,
@@ -23,29 +23,24 @@ from blueshift.api import(  symbol,
 
 
 class Signal:
-    STRONG_BUY = 10
     BUY = 1
-    STRONG_SELL = -10
     SELL = -1
     NO_SIGNAL = 999
     
     @classmethod
     def get_position_size(cls, signal):
-        if signal == cls.STRONG_BUY:
-            return 1
-        elif signal == cls.BUY:
+        if signal == cls.BUY:
             return 0.5
-        if signal == cls.STRONG_SELL:
-            return -1
         elif signal == cls.SELL:
             return -0.5
         return 0
 
 def initialize(context):
     # strategy parameters
-    context.params = {'lookback':375,
+    context.params = {'daily_lookback':20,
+                      'intraday_lookback':60,
                       'universe':['NIFTY-I','BANKNIFTY-I'],
-                      'ADX_period':120,
+                      'stoploss':0.005,
                       'leverage':2}
     
     if not context.params['universe']:
@@ -56,12 +51,22 @@ def initialize(context):
     set_commission(commission.PerShare(cost=0.002, min_trade_cost=0.0))
     set_slippage(slippage.FixedSlippage(0.00))
     
-    schedule_function(enter_trades, date_rules.every_day(),
+    schedule_function(strategy, date_rules.every_day(),
                       time_rules.every_nth_minute())
     schedule_function(stop_entry, date_rules.every_day(),
                       time_rules.market_close(hours=2))
     schedule_function(square_off_all, date_rules.every_day(),
                       time_rules.market_close(minutes=30))
+    
+def generate_supports(context, data):
+    cols = ['open','high','low','close']
+    lookback = context.params['daily_lookback']
+    ohlc = data.history(context.universe, cols, lookback, '1d')
+    
+    for asset in context.universe:
+        px = ohlc.xs(asset)
+        context.supports[asset] = [min(px) + l*(max(px) - min(px)) \
+                          for l in [0,0.236,0.382,0.5,0.618,1]]
     
 def before_trading_start(context, data):
     # reset all trackers
@@ -69,6 +74,9 @@ def before_trading_start(context, data):
     context.trade = True
     context.entered = {}
     context.exited = set()
+    context.supports = {}
+    context.signal = {}
+    generate_supports(context, data)
 
 def stop_entry(context, data):
     context.entry = False
@@ -82,23 +90,18 @@ def square_off_all(context, data):
         
     context.trade = False
 
-def enter_trades(context, data):
+def strategy(context, data):
     if not context.trade:
         return
     
-    cols = ['open','high','low','close']
-    lookback = context.params['lookback']
-    try:
-        ohlc = data.history(context.universe, cols, lookback, '1m')
-    except:
-        return
+    cols = ['close','volume']
+    lookback = context.params['intraday_lookback']
+    ohlc = data.history(context.universe, cols, lookback, '1m')
 
     for asset in context.universe:
         px = ohlc.xs(asset)
         if asset not in context.entered:
             check_entry(context, asset, px)
-        elif asset not in context.exited:
-            check_exit(context, asset, px)
         
 def check_entry(context, asset, px):
     if not context.entry or not context.trade:
@@ -107,7 +110,7 @@ def check_entry(context, asset, px):
     if asset in context.exited or asset in context.entered:
         return
     
-    signal = signal_function(px, context.params)
+    signal = signal_function(context, asset, px)
     if signal == Signal.NO_SIGNAL:
         return
     
@@ -115,51 +118,22 @@ def check_entry(context, asset, px):
     size = pos*context.params['leverage']/len(context.universe)
     order_target_percent(asset, size)
     context.entered[asset]=pos
-    set_stoploss(asset, 'PERCENT', 0.005, trailing=True, 
-                 on_stoploss=on_exit)
-    
-def check_exit(context, asset, px):
-    if not context.trade:
-        return
-    
-    if asset not in context.entered or asset in context.exited:
-        return
-    
-    pos = context.entered[asset]
-    signal = signal_function(px, context.params)
-    if pos > 0 and signal in (Signal.STRONG_SELL, Signal.SELL):
-        cancel_and_exit(context, asset)
-    elif pos < 0 and signal in (Signal.STRONG_BUY, Signal.BUY):
-        cancel_and_exit(context, asset)
+    set_stoploss(
+            asset, 'PERCENT', context.params['stoploss'], trailing=True, 
+            on_stoploss=on_exit)
         
 def on_exit(context, asset):
     context.exited.add(asset)
-            
-def cancel_and_exit(context, asset):
-    orders = context.open_orders_by_asset(asset)
-    for order_id in orders:
-        try:
-            cancel_order(order_id)
-        except:
-            pass
-            
-    positions = context.portfolio.positions
-    if asset in positions:
-        order_target_percent(asset, 0)
-        
-    on_exit(context, asset)
 
-def signal_function(px, params):
-    lower, upper = fibonacci_support(px.close.values)
-    ind2 = adx(px, params['ADX_period'])
-
-    if lower == -1:
-        return Signal.STRONG_SELL
-    elif upper == -1:
-        return Signal.STRONG_BUY
-#    elif upper > 0.02 and lower > 0 and upper/lower > 3 and ind2 < 20:
-#        return Signal.SELL
-#    elif lower > 0.02 and upper > 0 and lower/upper > 3 and ind2 < 20:
-#        return Signal.BUY
+def signal_function(context, asset, px):
+    typical = 0.33*(px.close[-1] + px.high[-1] + px.low[-1])
+    volume_signal = ta.SMA(px.volume, 10)[-1] > ta.SMA(px.volume, 30)[-1]
+    
+    if typical > context.supports[asset][-1] and volume_signal:
+        # break-out on the upside
+        return Signal.BUY
+    elif typical < context.supports[asset][0] and volume_signal:
+        # break-out on the downside
+        return Signal.SELL
     else:
         return Signal.NO_SIGNAL
