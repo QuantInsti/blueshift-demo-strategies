2d1b329a401e9a94ea786ba6238c4ef80df8c731
Prodipta Ghosh <prodiptag@gmail.com>
2022-08-03 03:59:25 +0530

    fix bug

--- nse/short_straddle_920.py
+++ nse/short_straddle_920.py
@@ -14,59 +14,63 @@ from blueshift.finance import slippage
 
 def initialize(context):
     context.params = {
-            'open':5,
-            'squareoff':30,
+            'entry':5,
+            'exit':30,
             'lots':1,
             'stoploss':0.4,
             'takeprofit':0.4
             }
     
     try:
-        context.params['open'] = int(context.params['open'])
-        assert context.params['lookback'] <= 15
-        assert context.params['lookback'] >= 5
+        context.params['entry'] = int(context.params['entry'])
+        assert context.params['entry'] <= 15
+        assert context.params['entry'] >= 5
     except:
-        msg = 'open must be an integer between 5 and 15 (minutes).'
+        msg = 'entry must be an integer between 5 and 15 (minutes).'
         raise ValueError(msg)
         
     try:
-        context.params['squareoff'] = int(context.params['squareoff'])
-        assert context.params['squareoff'] <= 15
-        assert context.params['squareoff'] >= 5
+        context.params['exit'] = int(context.params['exit'])
+        assert context.params['exit'] <= 60
+        assert context.params['exit'] >= 15
     except:
-        msg = 'squareoff must be an integer between 30 and 60 (minutes).'
+        msg = 'squareoff must be an integer between 15 and 60 (minutes).'
         raise ValueError(msg)
         
     try:
         sl = float(context.params['stoploss'])
         if sl < 0.2 or sl > 0.8:raise ValueError()
     except:
-        'stoploss must be a fraction between 0.2 (20%) to 0.90 (90%).'
+        'stoploss must be a fraction between 0.2 (20%) to 0.80 (80%).'
         raise ValueError(msg)
         
     try:
         tp = float(context.params['takeprofit'])
         if tp < 0.2 or tp > 0.8:raise ValueError()
     except:
-        'takeprofit must be a fraction between 0.2 (20%) to 0.90 (90%).'
+        'takeprofit must be a fraction between 0.2 (20%) to 0.80 (80%).'
         raise ValueError(msg)
     
     context.universe = [
                         symbol('NIFTY-W0CE+0'),
                         symbol('NIFTY-W0PE-0'),
                         ]
+    entry = context.params['entry']
+    exit_ = context.params['exit']
     schedule_function(
-            enter, date_rules.every_day(), time_rules.market_open(5))
+            enter, date_rules.every_day(), 
+            time_rules.market_open(entry))
     schedule_function(
-            close_out, date_rules.every_day(), time_rules.market_close(30))
-    set_slippage(slippage.NoSlippage())
+            close_out, date_rules.every_day(), 
+            time_rules.market_close(exit_))
+    
+def before_trading_start(context, data):
+    context.entered = set()
 
 def enter(context, data):
     close_out(context, data)
     for asset in context.universe:
         order_target(asset,-50)
-        set_stoploss(asset, 'PERCENT', context.params['stoploss'], 
-                trailing=False, on_stoploss=on_exit)
 
 def close_out(context, data):
     positions = context.portfolio.positions
@@ -75,4 +79,15 @@ def close_out(context, data):
             order_target(asset, 0)
             
 def set_targets(context, data):
-    pass
+    if len(context.universe) == len(context.entered):
+        return
+    
+    for asset in context.portfolio.positions:
+        if asset in context.entered:
+            continue
+        set_stoploss(asset, 'PERCENT', context.params['stoploss'])
+        set_takeprofit(asset, 'PERCENT', context.params['takeprofit'])
+        context.entered.add(asset)
+    
+def handle_data(context, data):
+    set_targets(context, data)
