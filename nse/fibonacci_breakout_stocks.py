dfd1b1bb0275069310b289f2d0dc4444a79b10fd
Prodipta Ghosh <prodiptag@gmail.com>
2022-08-02 23:46:46 +0530

    fix bug

--- nse/fibonacci_breakout_stocks.py
+++ nse/fibonacci_breakout_stocks.py
@@ -10,51 +10,84 @@
     Style tags: momentum and breakout.
     Asset class: Index Futures.
     Dataset: NSE
+    Risk: High
+    Minimum Capital: 300,000
 """
 import talib as ta
 
 from blueshift.finance import commission, slippage
 from blueshift.api import(  symbol,
-                            order_target_percent,
+                            order_target,
                             set_commission,
                             set_slippage,
                             schedule_function,
                             date_rules,
                             time_rules,
                             cancel_order,
+                            attach_pipeline,
+                            pipeline_output,
+                            get_datetime,
+                            set_long_only,
                             set_stoploss,
                             set_takeprofit,
+                            set_algo_parameters,
                        )
 
+from blueshift.pipeline import Pipeline
+from blueshift.errors import NoFurtherDataError
+from blueshift_library.pipelines.pipelines import period_returns
+from blueshift.pipeline.factors import AverageDollarVolume
 
 class Signal:
     BUY = 1
     SELL = -1
     NO_SIGNAL = 999
-    
-    @classmethod
-    def get_position_size(cls, signal):
-        if signal == cls.BUY:
-            return 0.5
-        elif signal == cls.SELL:
-            return -0.5
-        return 0
 
 def initialize(context):
+    context.strategy_name = 'Fibonacci Breakout Strategy (Futures)'
+    
     # strategy parameters
     context.params = {'daily_lookback':20,
-                      'universe':'NIFTY-I,BANKNIFTY-I',
+                      'stocks':[],
                       'stoploss':0.005,
                       'takeprofit':None,
                       'short_sma':10,
                       'long_sma':30,
-                      'leverage':2}
+                      'num_stocks':5,
+                      'universe':100,
+                      'order_size':1000}
     
-    if not context.params['universe']:
-        raise ValueError(f'universe not defined.')
-    context.params['universe'] = context.params['universe'].split(',')
-    if len(context.params['universe']) > 10:
-        raise ValueError(f'universe can be maximum 10 instruments.')
+    set_algo_parameters('params') # the attribute of context
+    
+    context.pipeline = False
+    if not context.params['stocks']:
+        try:
+            context.params['universe'] = int(context.params['universe'])
+            assert context.params['universe'] <= 500
+            assert context.params['universe'] >= 50
+        except:
+            msg = 'universe must be an integer between 50 and 500.'
+            raise ValueError(msg)
+        try:
+            context.params['num_stocks'] = int(context.params['num_stocks'])
+            assert context.params['num_stocks'] <= 20
+            assert context.params['num_stocks'] >= 2
+        except:
+            msg = 'num_stocks must be an integer between 2 and 20.'
+            raise ValueError(msg)
+        context.universe = []
+        context.pipeline = True
+    else:
+        context.universe = context.params['stocks'].split(',')
+        context.universe = [symbol(s) for s in context.universe]
+        
+    n = len(context.universe) if context.universe else context.params['num_stocks']
+    required = n*context.params['order_size']
+    capital = context.portfolio.starting_cash
+    if capital < required:
+        msg = f'Required capital is {required}, alloted {capital}, '
+        msg += f'please add more capital or reduce number of stocks.'
+        raise ValueError(msg)
     
     try:
         assert context.params['daily_lookback'] == int(context.params['daily_lookback'])
@@ -74,7 +107,7 @@ def initialize(context):
         msg += 'long term lookback must be greater than short term lookback.'
         raise ValueError(msg)
     else:
-        context.params['intraday_lookback'] = context.params['long_sma']+10
+        context.intraday_lookback = context.params['long_sma']*2
         
     if context.params['stoploss']:
         try:
@@ -95,8 +128,6 @@ def initialize(context):
             raise ValueError(msg)
     else:
         context.params['takeprofit'] = None
-        
-    context.universe = [symbol(sym) for sym in context.params['universe']]
 
     # set trading cost and slippage to zero
     set_commission(commission.PerShare(cost=0.002, min_trade_cost=0.0))
@@ -109,7 +140,52 @@ def initialize(context):
     schedule_function(square_off_all, date_rules.every_day(),
                       time_rules.market_close(minutes=30))
     
+    if context.pipeline:
+        attach_pipeline(make_strategy_pipeline(context), 
+            name='strategy_pipeline')
+    
+def make_strategy_pipeline(context):
+    pipe = Pipeline()
+
+    lookback = context.params['lookback']*21
+    top_n = context.params['universe']*21
+    dollar_volume_filter = AverageDollarVolume(
+            window_length=lookback).top(top_n)
+    
+    # compute past returns
+    momentum = period_returns(lookback)
+    pipe.add(momentum,'momentum')
+    pipe.set_screen(dollar_volume_filter)
+    return pipe
+
+def generate_pipeline_universe(context, data):
+    try:
+        pipeline_results = pipeline_output('strategy_pipeline')
+    except NoFurtherDataError:
+        context.universe = []
+        return
+    
+    n = context.params['num_stocks']
+    candidates = pipeline_results.dropna().sort_values('momentum')
+    size = len(candidates)
+    
+    if size == 0:
+        print(f'{get_datetime()}, no stocks passed filterting criteria.')
+        context.universe = []
+        return
+        
+    if size < n:
+        print(f'{get_datetime()}, only {size} stocks passed filterting criteria.')
+        
+    context.universe = candidates[-n:].index.tolist()
+    
 def generate_supports(context, data):
+    if context.pipeline:
+        generate_pipeline_universe()
+    
+    if not context.universe:
+        return
+    
     cols = ['close']
     lookback = context.params['daily_lookback']
     ohlc = data.history(context.universe, cols, lookback, '1d')
@@ -123,7 +199,7 @@ def before_trading_start(context, data):
     # reset all trackers
     context.entry = True
     context.trade = True
-    context.entered = {}
+    context.entered = set()
     context.exited = set()
     context.supports = {}
     context.signal = {}
@@ -137,17 +213,18 @@ def square_off_all(context, data):
         cancel_order(oid)
         
     for asset in context.portfolio.positions:
-        order_target_percent(asset, 0)
+        order_target(asset, 0)
         
     context.trade = False
 
 def strategy(context, data):
-    if not context.trade:
+    if not context.trade or not context.universe:
         return
     
-    cols = ['close','high','low','volume']
-    lookback = context.params['intraday_lookback']
-    ohlc = data.history(context.universe, cols, lookback, '1m')
+    #cols = ['close','high','low','volume']
+    cols = ['close','volume']
+    ohlc = data.history(
+            context.universe, cols, context.intraday_lookback, '1m')
 
     for asset in context.universe:
         px = ohlc.xs(asset)
@@ -165,10 +242,9 @@ def check_entry(context, asset, px):
     if signal == Signal.NO_SIGNAL:
         return
     
-    pos = Signal.get_position_size(signal)
-    size = pos*context.params['leverage']/len(context.universe)
-    order_target_percent(asset, size)
-    context.entered[asset]=pos
+    size = context.params['order_size']
+    order_target(asset, size)
+    context.entered.add(asset)
     
     if context.params['stoploss']:
         set_stoploss(
@@ -182,15 +258,15 @@ def on_exit(context, asset):
     context.exited.add(asset)
 
 def signal_function(context, asset, px):
-    typical = 0.33*(px.close[-1] + px.high[-1] + px.low[-1])
+    last = px.close[-1]
     t = context.params['short_sma']
     T = context.params['long_sma']
     volume_signal = ta.SMA(px.volume, t)[-1] > ta.SMA(px.volume, T)[-1]
     
-    if typical > context.supports[asset][-1] and volume_signal:
+    if last > context.supports[asset][-1] and volume_signal:
         # break-out on the upside
         return Signal.BUY
-    elif typical < context.supports[asset][0] and volume_signal:
+    elif last < context.supports[asset][0] and volume_signal:
         # break-out on the downside
         return Signal.SELL
     else:
