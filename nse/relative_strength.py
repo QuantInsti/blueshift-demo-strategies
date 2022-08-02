7b645e31517568a43b82b87a2acbd4240e5c50ba
Prodipta Ghosh <prodiptag@gmail.com>
2022-08-03 05:03:32 +0530

    fix bug

--- nse/relative_strength.py
+++ nse/relative_strength.py
@@ -8,19 +8,15 @@
         orders. Entry is allowed till 2 hours before the market closes. 
         All positions are squared off 30 minutes before the market closes.
     Style tags: momentum and breakout.
-    Asset class: Index Futures.
+    Asset class: Equties and ETFs.
     Dataset: NSE
     Risk: High
     Minimum Capital: 300,000
 """
 import numpy as np
-import talib as ta
 
-from blueshift.finance import commission, slippage
 from blueshift.api import(  symbol,
                             order_target,
-                            set_commission,
-                            set_slippage,
                             schedule_function,
                             date_rules,
                             time_rules,
@@ -28,7 +24,6 @@ from blueshift.api import(  symbol,
                             attach_pipeline,
                             pipeline_output,
                             get_datetime,
-                            set_long_only,
                             set_stoploss,
                             set_takeprofit,
                             set_algo_parameters,
@@ -40,40 +35,26 @@ from blueshift.errors import NoFurtherDataError
 from blueshift.pipeline.factors import AverageDollarVolume
 from blueshift.pipeline import CustomFactor
 from blueshift.pipeline.data import EquityPricing
+from blueshift_library.technicals.indicators import rsi
 
 class Signal:
-    STRONG_BUY = 1
-    BUY = 0.5
-    SELL = -0.5
-    STRONG_SELL = -1
+    BUY = 1
+    SELL = -1
     NO_SIGNAL = 0
-    
-def atr_factor(window_length, lookback):
-    class ATR(CustomFactor):
-        inputs = [EquityPricing.close, EquityPricing.high, EquityPricing.low]
-        def compute(self,today,assets,out,close,high,low):
-            for i in range(close.shape[1]):
-                try:
-                    atr = ta.ATR(
-                            high[:,i],low[:,i],close[:,i],lookback)
-                    out[i] = atr[-1]
-                except:
-                    out[i] = np.nan
-    return ATR(window_length = window_length)
 
-def move_factor(window_length, lookback):
+def rs_factor(window_length):
     class Move(CustomFactor):
         inputs = [EquityPricing.close]
         def compute(self,today,assets,out,close):
-            out[:] = (close[-1] - close[0])/window_length
+            out[:] = 100*(close[-1]/close[0])
     return Move(window_length = window_length)
 
 def initialize(context):
     context.strategy_name = 'Fibonacci Breakout Strategy (Stocks)'
     
     # strategy parameters
-    context.params = {'daily_lookback':20,
-                      'stocks':[],
+    context.params = {'daily_lookback':60,
+                      'rsi_period':60,
                       'stoploss':0.005,
                       'takeprofit':None,
                       'num_stocks':5,
@@ -82,46 +63,34 @@ def initialize(context):
     
     set_algo_parameters('params') # the attribute of context
     
-    context.pipeline = False
-    if not context.params['stocks']:
-        try:
-            context.params['universe'] = int(context.params['universe'])
-            assert context.params['universe'] <= 500
-            assert context.params['universe'] >= 50
-        except:
-            msg = 'universe must be an integer between 50 and 500.'
-            raise ValueError(msg)
-        try:
-            context.params['num_stocks'] = int(context.params['num_stocks'])
-            assert context.params['num_stocks'] <= 20
-            assert context.params['num_stocks'] >= 2
-        except:
-            msg = 'num_stocks must be an integer between 2 and 20.'
-            raise ValueError(msg)
-        context.universe = []
-        context.pipeline = True
-    else:
-        context.universe = context.params['stocks'].split(',')
-        context.universe = [symbol(s) for s in context.universe]
-        
-    n = len(context.universe) if context.universe else context.params['num_stocks']
-    required = n*context.params['order_size']
-    capital = context.portfolio.starting_cash
-    if capital < required:
-        msg = f'Required capital is {required}, alloted {capital}, '
-        msg += f'please add more capital or reduce number of stocks.'
-        raise ValueError(msg)
-    
     try:
-        assert context.params['daily_lookback'] == int(context.params['daily_lookback'])
-        assert context.params['daily_lookback'] > 10
-        assert context.params['daily_lookback'] <= 60
+        context.params['universe'] = int(context.params['universe'])
+        assert context.params['universe'] <= 500
+        assert context.params['universe'] >= 50
     except:
-        msg = 'daily lookback must be integer and greater than 10 and '
-        msg += 'less than or equal to 60.'
+        msg = 'universe must be an integer between 50 and 500.'
         raise ValueError(msg)
-        
-    context.intraday_lookback = 10 # we use last 5 bars only
+    try:
+        context.params['num_stocks'] = int(context.params['num_stocks'])
+        assert context.params['num_stocks'] <= 20
+        assert context.params['num_stocks'] >= 2
+    except:
+        msg = 'num_stocks must be an integer between 2 and 20.'
+        raise ValueError(msg)
+    try:
+        context.params['rsi_period'] = int(context.params['rsi_period'])
+        assert context.params['rsi_period'] <= 120
+        assert context.params['rsi_period'] >= 15
+    except:
+        msg = 'rsi_period must be an integer between 15 and 120 (minutes).'
+        raise ValueError(msg)
+    try:
+        context.params['daily_lookback'] = int(context.params['daily_lookback'])
+        assert context.params['daily_lookback'] <= 200
+        assert context.params['daily_lookback'] >= 20
+    except:
+        msg = 'daily_lookback must be an integer between 20 and 200 (days).'
+        raise ValueError(msg)
         
     if context.params['stoploss']:
         try:
@@ -142,10 +111,39 @@ def initialize(context):
             raise ValueError(msg)
     else:
         context.params['takeprofit'] = None
-
-    # set trading cost and slippage to zero
-    set_commission(commission.PerShare(cost=0.002, min_trade_cost=0.0))
-    set_slippage(slippage.FixedSlippage(0.00))
+        
+    context.long_universe = []
+    context.short_universe = []
+        
+    n = 2*context.params['num_stocks']
+    required = n*context.params['order_size']
+    capital = context.portfolio.starting_cash
+    if capital < required:
+        msg = f'Required capital is {required}, alloted {capital}, '
+        msg += f'please add more capital or reduce number of stocks.'
+        raise ValueError(msg)
+        
+    context.intraday_lookback = 2*context.params['rsi_period']
+        
+    if context.params['stoploss']:
+        try:
+            sl = float(context.params['stoploss'])
+            if sl < 0 or sl > 0.1:raise ValueError()
+        except:
+            'stoploss must be a fraction between 0 to 0.10'
+            raise ValueError(msg)
+    else:
+        context.params['stoploss'] = None
+            
+    if context.params['takeprofit']:
+        try:
+            tp = float(context.params['takeprofit'])
+            if tp < 0.005 or tp > 0.1:raise ValueError()
+        except:
+            'takeprofit must be a fraction between 0.005 to 0.10'
+            raise ValueError(msg)
+    else:
+        context.params['takeprofit'] = None
     
     schedule_function(strategy, date_rules.every_day(),
                       time_rules.every_nth_minute())
@@ -158,7 +156,7 @@ def initialize(context):
         attach_pipeline(make_strategy_pipeline(context), 
             name='strategy_pipeline')
     
-    #set_long_only()
+    context.benchmark = symbol('NIFTY50')
     msg = f'Starting strategy {context.strategy_name} '
     msg += f'with parameters {context.params}'
     print(msg)
@@ -172,52 +170,40 @@ def make_strategy_pipeline(context):
             window_length=lookback).top(top_n)
     
     # compute atr
-    atr = atr_factor(2*lookback, lookback)
-    move = move_factor(2*lookback, lookback)
-    pipe.add(atr,'atr')
+    move = rs_factor(window_length=lookback)
     pipe.add(move,'move')
     pipe.set_screen(dollar_volume_filter)
     return pipe
 
-def generate_pipeline_universe(context, data):
+def generate_universe(context, data):
     try:
         pipeline_results = pipeline_output('strategy_pipeline')
     except NoFurtherDataError:
-        context.universe = []
+        context.long_universe = []
+        context.short_universe = []
         return
     
     n = context.params['num_stocks']
+    lookback = context.params['daily_lookback']
+    benchmark = data.history(context.benchmark,'close',lookback,'1d')
+    move = 100*(benchmark[-1]/benchmark[0])
+    
     candidates = pipeline_results.dropna()
-    metric = candidates.atr/candidates.move.abs()
+    metric = candidates.move/move
     candidates = metric.sort_values()
-    size = len(candidates)
+    size = int(len(candidates)/2)
     
     if size == 0:
         print(f'{get_datetime()}, no stocks passed filterting criteria.')
-        context.universe = []
+        context.long_universe = []
+        context.short_universe = []
         return
         
     if size < n:
         print(f'{get_datetime()}, only {size} stocks passed filterting criteria.')
-        
-    # choose stocks with highest true ranges to move ratio
-    context.universe = candidates[-n:].index.tolist()
     
-def generate_supports(context, data):
-    if context.pipeline:
-        generate_pipeline_universe(context, data)
-        
-    if not context.universe:
-        return
-    
-    cols = ['close']
-    lookback = context.params['daily_lookback']
-    ohlc = data.history(context.universe, cols, lookback, '1d')
-    
-    for asset in context.universe:
-        px = ohlc.xs(asset).close
-        context.supports[asset] = [min(px) + l*(max(px) - min(px)) \
-                          for l in [0,0.236,0.382,0.5,0.618,1]]
+    context.long_universe = candidates[-size:].index.tolist()
+    context.short_universe = candidates[:size].index.tolist()
     
 def before_trading_start(context, data):
     # reset all trackers
@@ -227,7 +213,7 @@ def before_trading_start(context, data):
     context.exited = set()
     context.supports = {}
     context.signal = {}
-    generate_supports(context, data)
+    generate_universe(context, data)
 
 def stop_entry(context, data):
     context.entry = False
@@ -240,15 +226,18 @@ def square_off_all(context, data):
     context.trade = False
 
 def strategy(context, data):
-    if not context.trade or not context.universe:
+    if not context.trade:
         return
     
-    cols = ['close','high','low']
-    ohlc = data.history(
-            context.universe, cols, context.intraday_lookback, '1m')
+    if not context.long_universe and not context.short_universe:
+        return
+    
+    universe = list(set(context.long_universe + context.short_universe))
+    cols = 'close'
+    ohlc = data.history(universe, cols, context.intraday_lookback, '1m')
 
-    for asset in context.universe:
-        px = ohlc.xs(asset)
+    for asset in universe:
+        px = ohlc[asset]
         if asset not in context.entered:
             check_entry(context, asset, px)
         
@@ -276,25 +265,14 @@ def check_entry(context, asset, px):
                        on_takeprofit=on_exit)
         
 def on_exit(context, asset):
-    print(f'{get_datetime()}:exiting asset {asset}.')
     context.exited.add(asset)
 
 def signal_function(context, asset, px):
-    last, prev = px.close[-1], px.close[-2]
-    high, low = np.nanmax(px.high[-5:]), np.nanmin(px.high[-5:])
-    s1, s2 = context.supports[asset][0], context.supports[asset][1]
-    r1, r2 = context.supports[asset][-2], context.supports[asset][-1]
+    sig = rsi(px, context.params['rsi_period'])
     
-    # check if we have a pullback from the supports
-    if low < s1 and last > s1 and prev > s1:
-        # pullback from low
-        return Signal.STRONG_SELL
-    elif high > r2 and last < r2 and prev < r2:
-        # pullback from high
-        return Signal.STRONG_BUY
-    elif low < s2 and last > s2 and prev > s2:
-        return Signal.SELL
-    elif high > r1 and last < r1 and prev < r1:
+    if sig > 65 and asset in context.long_universe:
         return Signal.BUY
+    elif sig < 35 and asset in context.short_universe:
+        return Signal.SELL
     else:
         return Signal.NO_SIGNAL
--- nse/relative_strength_breakout.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug  1 22:44:29 2022
-
-@author: prodi
-"""
-
